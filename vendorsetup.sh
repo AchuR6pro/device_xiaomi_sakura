48eab070aa089c68af08c1bc094f2a89385e1bcd
achu.exe <152979349+AchuR6pro@users.noreply.github.com>
2024-05-08 14:57:40 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,9 @@
+# Common Tree
+git clone https://github.com/AchuR6pro/device_xiaomi_msm8953.git -b lineage-21 device/xiaomi/msm8953-common
+
 # Vendor Tree
-rm -rf vendor/xiaomi
 git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_daisy.git -b lineage-21 vendor/xiaomi/daisy
 git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_msm8953-common.git -b lineage-21 vendor/xiaomi/msm8953-common
 
 # Kernel Tree
-rm -rf kernel/xiaomi
 git clone https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953.git -b lineage-21 kernel/xiaomi/msm8953
